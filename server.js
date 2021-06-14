6b387083a6df47681bed2e045ad618bde9cc390c
Fernando jose Mejia <>
2021-06-14 09:59:56 -0400

    ya no se que hacer

--- server.js
+++ server.js
@@ -22,10 +22,7 @@ db.sequelize.sync();
 
 app.get('/', (req, res) => {
     res.send('Server is ready');
-    var result = 'App is running'
-  response.send(result);
-}).listen(app.get('port'), function() {
-  console.log('App is running, server is listening on port ', app.get('port'));
+ 
 
 });
 
